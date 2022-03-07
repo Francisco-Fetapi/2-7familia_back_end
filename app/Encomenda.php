cb437561d7e03784847ae093bf3fc78d3cb916ba
Edito Donato <editotchocosso13@gmail.com>
2022-03-08 00:53:35 +0100

    Criando o relacionamento na tabela Encomendas

--- app/Encomenda.php
+++ app/Encomenda.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Encomenda extends Model
 {
-    //
+    protected $table = 'encomendas';
+
+    public function usuarios(){
+        return $this->hasOne('App\User','id_usuario','id');
+    }
+    public function produtos(){
+        return $this->hasOne('App\Produto','id_produto','id');
+    }
 }
