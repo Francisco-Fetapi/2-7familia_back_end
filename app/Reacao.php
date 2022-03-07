35a98d679e86e4a64fed86ffb2948a2ef1f37f79
Edito Donato <editotchocosso13@gmail.com>
2022-03-08 00:54:39 +0100

    Criando o relacionamento na tabela Reacaos

--- app/Reacao.php
+++ app/Reacao.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reacao extends Model
 {
-    //
+    protected $table = 'reacaos';
+
+    public function usuarios(){
+        return $this->hasOne('App\User','id_usuario','id');
+    }
+    public function produtos(){
+        return $this->hasOne('App\Produto','id_produto','id');
+    }
 }
